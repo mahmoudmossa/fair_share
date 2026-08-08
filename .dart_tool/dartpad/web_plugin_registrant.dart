0dffe754dc0677b2e876b5f5807b11e8e6a0876b
Ahmed <mahmed@emtec.local>
2026-08-08 19:11:51 +0200

    fix: sematic release

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -9,10 +9,10 @@
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
-import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -21,10 +21,10 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
   FlutterImageCompressWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
-  FirebaseMessagingWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
